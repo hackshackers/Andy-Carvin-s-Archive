8f8fb74f7d379942b7a93d49c1a85691036dba45
Sisi Wei <me@sisiwei.com>
2011-11-13 17:22:47 -0500

    End of Hacks/Hackers progress

---
+++ schema.js
@@ -0,0 +1,18 @@
+{items: [
+    {type: "location",
+     label: "bridge",
+     latlng: "30.043747,31.229464"},
+    {type: "location",
+     label: "museum",
+     latlng: "30.047778,31.233333"},
+    {type: "location",
+     label: "talaat harb",
+     latlng: "30.044939,31.23680"},
+    {type: "location",
+     label: "Omar Makram",
+     latlng: "30.045360,31.23670"},
+    {type: "location",
+     label: "tahrir",
+     latlng: "30.044422,31.235696"}
+]
+}
